654fe78a36a3e5c3ee9290059653c727dd211b77
chetanya-rohilla <chetanyarohilla99@gmail.com>
2022-06-22 13:41:56 +0530

    Update API Done

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -23,19 +23,30 @@ const updateBlog = async function(req, res) {
     try {
         const blogId = req.params.blogId
         const blog = await blogModel.findById(blogId)
-    
+
         if(!blog)   return res.status(404).send({status : false, msg : "Blog Id is incorrect"})
         if(blog.isDeleted == true)  return res.status(404).send({status : false, msg : "Blog doesn't exist"})
         if(Object.keys(req.body).length == 0)   return res.status(400).send({status : false, msg : "Empty body for update"})
     
+        for(const key in req.body){
+            console.log(key);
+            if(typeof (req.body[key]) == "object"){
+                req.body[key].push(...blog[key]);
+            }
+        }
+
         let query = {...req.body, isPublished : true, publishedAt : moment().format('YYYY-MM-DDTss:mm:h')}
-        const updatedBlog = await blogModel.findOneAndUpdate({blogId : blogId}, query, {new : true})
-    
+        const updatedBlog = await blogModel.findOneAndUpdate({_id : blogId}, query, {new : true})
+
         return res.status(200).send ({status: true, data: updatedBlog });
     } catch (error) {
         return res.status(500).send ({status: false, msg: error.message });
     }
 }
+
+module.exports.createNewBlog = createNewBlog
+module.exports.updateBlog = updateBlog
+module.exports.deleteBlog = deleteBlog
 
 module.exports = {
     createNewBlog,
