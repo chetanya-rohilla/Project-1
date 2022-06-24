00b0edb5242b8640cdd6f8655b15b06d7f103ad3
mantoopandey <vikaspandey42@gmail.com>
2022-06-24 16:55:13 +0530

    comments

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,6 +1,7 @@
 const authorModel = require("../models/authorModel");
 const jwt = require("jsonwebtoken");
 
+//  validations for strings and objects ,checking for empty string by using trim
 
 const typeChecking = function(data){
     if(typeof data !== 'string' && typeof data !== "object"){
@@ -18,7 +19,12 @@ const createAuthor = async function (req, res) {
         let data = req.body;
         let { fname, lname, title, email, password } = data;
 
+        // Object.keys() will give an array of keys inside req.body. We will check if the length of this array is 0 ie nothing is passed in body.
+
         if (Object.keys(req.body).length == 0)   return res.status(400).send({status : false, msg : "No information passed"})
+
+        // checking if the mandatory field are present or not 
+
         if (!fname) return res.status(400).send({ status: false, msg: "First Name is required...!" });
         if (!typeChecking(fname)) return res.status(400).send({ status: false, msg: "Please enter the first name in right format...!" });
         if (!lname) return res.status(400).send({ status: false, msg: "Last name is required...!" });
@@ -30,7 +36,11 @@ const createAuthor = async function (req, res) {
         if (!password) return res.status(400).send({ status: false, msg: "Password is required...!" });
         if (!typeChecking(password)) return res.status(400).send({ status: false, msg: "Please enter the password in right format...!" });
 
+       //  email format validation by using Regex
+
         if (!email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) return res.status(400).send({ status: false, msg: "Wrong Email format" })
+
+        // Enum validation by using a array 
 
         const allowed = ["Mr", "Miss", "Mrs"]
         if (!allowed.includes(title)) return res.status(400).send({ status: false, msg: "Invalid Title, Select one from Mr, Mrs or Miss" });
@@ -56,6 +66,8 @@ const login = async function (req, res) {
 
         let author = await authorModel.findOne({ email: email, password: pass });
         if (!author) return res.status(401).send({ status: false, msg: "Email or Password is wrong" });
+
+        // secret key is the initials of each group member.
 
         let token = jwt.sign(
             {
