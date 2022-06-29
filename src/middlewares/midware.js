3fadc6d31e412ac5f7f1af1193214b966bfe1965
chetanya-rohilla <chetanyarohilla99@gmail.com>
2022-06-29 19:26:15 +0530

    token validation

--- src/middlewares/midware.js
+++ src/middlewares/midware.js
@@ -1,22 +1,20 @@
 const jwt = require('jsonwebtoken')
 
-const auth = async function(req, res, next) {
+const auth = async function (req, res, next) {
     try {
         const token = req.headers['x-api-key']
-        if (!token)     return res.status(401).send({status: false, msg: "Please provide token" })
-        const validToken = jwt.verify(token, "VRCA")
+        if (!token) return res.status(401).send({ status: false, msg: "Please provide token" })
+        jwt.verify(token, "VRCA", (err, ) => {
+            if (err) return res.status(401).sendecoded({ status: false, msg: "Token is Incorrect" })
+            req.validToken = decode
+            next()
+        })
 
         // Passing the decoded token inside req to acces it in controllers for authorisation.
 
-        req.validToken = validToken
-        next()
+
     } catch (error) {
-
-        // Handling errors related to jwt token.
-
-        if(error.message == "jwt malformed")    return res.status(401).send({status: false, msg: "Token is Incorrect" })
-        if(error.message == "invalid token")    return res.status(401).send({status: false, msg: "Token is Incorrect" })
-        return res.status(500).send ({status: false, msg: error.message });
+        return res.status(500).send({ status: false, msg: error.message });
     }
 }
 
