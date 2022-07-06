df8a92490e305eda3beda153c2635047bc805f76
chetanya-rohilla <102756843+chetanya-rohilla@users.noreply.github.com>
2022-07-06 13:24:13 +0530

    Update midware.js

--- src/middlewares/midware.js
+++ src/middlewares/midware.js
@@ -4,7 +4,7 @@ const auth = async function (req, res, next) {
     try {
         const token = req.headers['x-api-key']
         if (!token) return res.status(401).send({ status: false, msg: "Please provide token" })
-        jwt.verify(token, "VRCA", (err, ) => {
+        jwt.verify(token, "VRCA", (err, decode) => {
             if (err) return res.status(401).sendecoded({ status: false, msg: "Token is Incorrect" })
             req.validToken = decode
             next()
